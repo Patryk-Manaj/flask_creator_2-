51065d5a96fcb831c9b48105fc04736a9b6fd920
Patryk-Manaj <102524718+Patryk-Manaj@users.noreply.github.com>
2022-04-23 15:27:36 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -262,7 +262,7 @@ def translate_declaration(filename):
 
     pdf.add_font('Siemens', '', r'/home/manajpatryk/app/app/fonts/Dialog-Boldd.ttf', uni=True)
     pdf.add_font('Open_Sans', '', r'/home/manajpatryk/app/app/fonts/OpenSans-Italic-VariableFont_wdth.ttf', uni=True)
-    pdf.add_font('Robo', '',r'/home/manajpatryk/app/app/fonts/Roboto-Regularr.ttf', uni=True)
+    pdf.add_font('Robo', '',r'/home/manajpatryk/app/app/fonts/Roboto-Regular.ttf', uni=True)
 
     pdf.set_font('Siemens', '', 28)
 

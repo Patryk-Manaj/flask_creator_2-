40210ce9d72dc133d1832a7b1d23c5d273e5d4de
Patryk-Manaj <102524718+Patryk-Manaj@users.noreply.github.com>
2022-07-22 14:08:16 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -110,6 +110,10 @@ def upload_pdf():
     if request.method == "POST":
 
         if request.files:
+            
+            if not os.listdir("/home/pama/app/app/static/pdf/downloads/") == ""
+                rm_path_down = "/home/pama/app/app/static/pdf/downloads/" + os.listdir
+                os.remove(rm_path_down)
 
             if not allowed_pdf_filesize(request.cookies.get("filesize")):
                 flash("Plik jest za duży!")
@@ -142,10 +146,7 @@ def upload_pdf():
             else:
                 try:
                     file_name = request.form['file_name'] + ".pdf"
-                    send_from_directory(app.config["PDF_DOWNLOADS"], file_name, as_attachment=True)
-                    time.sleep(3)
-                    rm_path_down = "/home/pama/app/app/static/pdf/downloads/" + file_name
-                    os.remove(rm_path_down)
+                    return send_from_directory(app.config["PDF_DOWNLOADS"], file_name, as_attachment=True)
                 except FileNotFoundError:
                     os.abort(404)
             # return redirect(request.url)
